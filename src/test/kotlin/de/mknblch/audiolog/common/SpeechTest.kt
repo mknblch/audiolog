48ca8fc851f9ed38d5d435ce0f385d69b8d6c6e3
mknblch <mknblch@gmail.com>
2022-05-26 13:16:25 +0200

    wip

--- src/test/kotlin/de/mknblch/audiolog/common/SpeechTest.kt
+++ src/test/kotlin/de/mknblch/audiolog/common/SpeechTest.kt
@@ -1,53 +1,53 @@
-package de.mknblch.audiolog.common
-
-import org.junit.jupiter.api.Test
-import java.util.*
-import javax.speech.Central
-import javax.speech.synthesis.Synthesizer
-import javax.speech.synthesis.SynthesizerModeDesc
-
-
-class SpeechTest {
-
-    @Test
-    fun test() {
-        try {
-            // Set property as Kevin Dictionary
-            System.setProperty(
-                "freetts.voices", "com.sun.speech.freetts.en.us"
-                        + ".cmu_us_kal.KevinVoiceDirectory"
-            )
-
-            // Register Engine
-            Central.registerEngineCentral(
-                "com.sun.speech.freetts"
-                        + ".jsapi.FreeTTSEngineCentral"
-            )
-
-            // Create a Synthesizer
-            val synthesizer = Central.createSynthesizer(
-                SynthesizerModeDesc(Locale.US)
-            )
-
-            // Allocate synthesizer
-            synthesizer.allocate()
-
-            // Resume Synthesizer
-            synthesizer.resume()
-
-            // Speaks the given text
-            // until the queue is empty.
-            synthesizer.speakPlainText(
-                "Velketor the Sorcerer's spell interrupted", null
-            )
-            synthesizer.waitEngineState(
-                Synthesizer.QUEUE_EMPTY
-            )
-
-            // Deallocate the Synthesizer.
-            synthesizer.deallocate()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-}
+//package de.mknblch.audiolog.common
+//
+//import org.junit.jupiter.api.Test
+//import java.util.*
+//import javax.speech.Central
+//import javax.speech.synthesis.Synthesizer
+//import javax.speech.synthesis.SynthesizerModeDesc
+//
+//
+//class SpeechTest {
+//
+//    @Test
+//    fun test() {
+//        try {
+//            // Set property as Kevin Dictionary
+//            System.setProperty(
+//                "freetts.voices", "com.sun.speech.freetts.en.us"
+//                        + ".cmu_us_kal.KevinVoiceDirectory"
+//            )
+//
+//            // Register Engine
+//            Central.registerEngineCentral(
+//                "com.sun.speech.freetts"
+//                        + ".jsapi.FreeTTSEngineCentral"
+//            )
+//
+//            // Create a Synthesizer
+//            val synthesizer = Central.createSynthesizer(
+//                SynthesizerModeDesc(Locale.US)
+//            )
+//
+//            // Allocate synthesizer
+//            synthesizer.allocate()
+//
+//            // Resume Synthesizer
+//            synthesizer.resume()
+//
+//            // Speaks the given text
+//            // until the queue is empty.
+//            synthesizer.speakPlainText(
+//                "Velketor the Sorcerer's spell interrupted", null
+//            )
+//            synthesizer.waitEngineState(
+//                Synthesizer.QUEUE_EMPTY
+//            )
+//
+//            // Deallocate the Synthesizer.
+//            synthesizer.deallocate()
+//        } catch (e: Exception) {
+//            e.printStackTrace()
+//        }
+//    }
+//}
